ec37296d9d3b149e8b2e693b74e971fafca95050
Sarah <vanwars@gmail.com>
2023-03-22 08:11:47 -0400

    updated course files

--- course-files/lectures/lecture17/03-drawings/sketch.js
+++ course-files/lectures/lecture17/03-drawings/sketch.js
@@ -4,6 +4,20 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
     background("pink");
 
+    /* Use the drawing functions to draw a picture. Ideas:
+        * Draw an animal
+        * Draw a tree
+        * Draw a car
+        * Draw some abstract art
+    */
+
+    // https://p5js.org/reference/#/p5/circle
+    fill("teal")
+    circle(550, 100, 100);
+
+    // https://p5js.org/reference/#/p5/rect
+    fill('hotpink')
+    rect(30, 20, 55, 55);
 
     // add your drawing here:
     // https://p5js.org/reference/#/p5/point
@@ -12,18 +26,14 @@ function setup() {
     // https://p5js.org/reference/#/p5/text
     // text("Here is some text", 200, 40);
 
-    // https://p5js.org/reference/#/p5/circle
-    // fill('red');
-    // circle(550, 100, 100);
-
     // https://p5js.org/reference/#/p5/ellipse
-    // fill('teal');
+    // fill('red');
     // ellipse(100, 200, 60, 100);
 
     // Other shapes...
     // Polygon: https://p5js.org/reference/#/p5/beginShape
     // Line: https://p5js.org/reference/#/p5/line
-    // Rectangle: https://p5js.org/reference/#/p5/rect
+    
     // Curve: https://p5js.org/reference/#/p5/curve
 
 
